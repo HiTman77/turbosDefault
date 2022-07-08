6b81d15b65ce010bf2cdaf7baa5600642de741d8
HiTman77 <doug@kadabra.co.za>
2022-07-08 08:31:41 +0200

    d

--- theme.php
+++ theme.php
@@ -8,9 +8,9 @@
 <title><?= $Wcms->page('title') ?> - <?= $Wcms->get('config', 'siteTitle') ?></title>
 <meta name="description" content="<?= $Wcms->page('description') ?>">
 <meta name="keywords" content="<?= $Wcms->page('keywords') ?>">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <?= $Wcms->css() ?>
-<link rel="stylesheet" href="<?= $Wcms->asset('css/style.css') ?>?v=11">
+<link rel="stylesheet" rel="preload" as="style" href="<?= $Wcms->asset('css/style.css') ?>?v<?php echo(rand(1,33));?><?php echo(rand(1,20));?>">
 </head>
 <body>
 <?= $Wcms->settings() ?>
